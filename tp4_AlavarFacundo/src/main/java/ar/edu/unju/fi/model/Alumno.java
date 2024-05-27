079cc865ba4b85f9b0f8eee709107a0305850c7e
f4cu22 <fascundo96@gmail.com>
2024-05-26 23:50:15 -0300

    creacion de atributos, getters y setters

--- tp4_AlavarFacundo/src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ tp4_AlavarFacundo/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -1,5 +1,65 @@
 package ar.edu.unju.fi.model;
 
-public class Alumno {
+import java.sql.Date;
 
+public class Alumno {
+	private String codigo;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private Date fechaNacimiento;
+	private String domicilio;
+	private String LU;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getDomicilio() {
+		return domicilio;
+	}
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+	public String getLU() {
+		return LU;
+	}
+	public void setLU(String lU) {
+		LU = lU;
+	}
+	
+	
 }
